b85907a0a96054c28bded48eb06bbc0272764703
Muhammed Saryyew <nsaryyev9797@gmail.com>
2021-11-05 17:03:01 +0200

    fixed

--- src/app/slider.js
+++ src/app/slider.js
@@ -1,14 +1,18 @@
 import $ from "jquery";
 
 export default (function () {
+  const slider = $(".modal-work__slider");
+  slider.on("click", (event) => {
+    event.stopPropagation();
+  });
   // Slider: https://kenwheeler.github.io/slick/
-  $("#worksSlider").slick({
+  slider.slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     fade: false,
     arrows: true,
-    dots: true,
+    dots: true
   });
 
   const slickButtonBack = $("#slick-slide-control00");
